1c6ce135ce8901c2bedaa6d61ee331a0e80a416f
arturexpone <arturexpone@yandex.ru>
2020-06-15 17:32:37 +0300

    add drag-drop functionality

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ function createList() {
     .sort((a, b) => a.sort - b.sort)
     .map(a => a.value)
     .forEach((person, i) => {
-      console.log(person)
+
       const listItem = document.createElement('li');
 
       listItem.setAttribute('data-index', i);
@@ -36,7 +36,58 @@ function createList() {
         `;
 
       listItems.push(listItem);
-
       draggable_list.appendChild(listItem);
     });
 }
+
+function dragStart() {
+  dragStarIndex = +this.closest('li').getAttribute('data-index');
+}
+
+
+function dragEnter() {
+  this.classList.add('over');
+}
+
+function dragLeave() {
+  this.classList.remove('over');
+}
+
+function dragOver(e) {
+  e.preventDefault();
+}
+
+function dragDrop() {
+  const dragEndIndex = this.getAttribute('data-index');
+  swapItems(dragStarIndex, dragEndIndex);
+
+  this.classList.remove('over');
+}
+
+function swapItems(fromIndex, toIndex) {
+  const itemOne = listItems[fromIndex].querySelector('.draggable');
+  const itemTwo = listItems[toIndex].querySelector('.draggable');
+
+  listItems[fromIndex].appendChild(itemTwo);
+  listItems[toIndex].appendChild(itemOne);
+}
+
+function addEventListeners() {
+  const draggables = document.querySelectorAll('.draggable');
+  const dragListItems = document.querySelectorAll('.draggable-list li');
+
+  draggables.forEach(draggable => {
+    draggable.addEventListener('dragstart', dragStart);
+  });
+
+  dragListItems.forEach(item => {
+    item.addEventListener('dragover', dragOver);
+    item.addEventListener('drop', dragDrop);
+    item.addEventListener('dragenter', dragEnter);
+    item.addEventListener('dragleave', dragLeave);
+
+  });
+
+}
+
+addEventListeners();
